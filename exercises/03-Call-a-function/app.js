6c5255cce0b7bfcb77a5b874ae52b73e94d0c36c
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-22 04:39:48 +0000

    ok

--- exercises/03-Call-a-function/app.js
+++ exercises/03-Call-a-function/app.js
@@ -5,3 +5,6 @@ function calculateArea(length,edge)
 // Your code Below this line:
 function squareArea(length,edge)
 
+let squareArea = calculateArea(4,4)
+let squareArea = calculateArea(2,2)
+let squareArea = calculateArea(5,5)
